f3667c4afb479ab89ec69a6996ef244ccc66f57a
pupsich <asndjabbd@gmail.com>
2022-04-03 21:40:48 +0300

    TV show details activity information added

--- app/src/main/java/com/example/pepflix/ui/TVShowDetailsActivity.kt
+++ app/src/main/java/com/example/pepflix/ui/TVShowDetailsActivity.kt
@@ -1,13 +1,15 @@
 package com.example.pepflix.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
@@ -15,6 +17,7 @@ import com.example.pepflix.R
 import com.example.pepflix.adapters.ImageSliderAdapter
 import com.example.pepflix.databinding.ActivityTvshowDetailsBinding
 import com.example.pepflix.models.TVShow
+import com.example.pepflix.utilities.BindingAdapters
 import com.example.pepflix.viewmodels.TVShowDetailsViewModel
 
 class TVShowDetailsActivity : AppCompatActivity() {
@@ -33,6 +36,11 @@ class TVShowDetailsActivity : AppCompatActivity() {
 
     private fun doInitialization() {
         tvShowDetailsViewModel = ViewModelProvider(this)[TVShowDetailsViewModel::class.java]
+
+        binding.ivBackButton.setOnClickListener {
+            onBackPressed()
+        }
+
         getTVShowDetails()
     }
 
@@ -43,6 +51,46 @@ class TVShowDetailsActivity : AppCompatActivity() {
             binding.pbTvShowDetails.visibility = View.GONE
             if (response != null) {
                 loadImageSlider(response.tvShowDetails.pictures)
+                BindingAdapters.setImageURL(binding.rivTVShowImage, response.tvShowDetails.imagePath)
+                binding.rivTVShowImage.visibility = View.VISIBLE
+                binding.tvTVShowDescription.text = HtmlCompat.fromHtml(
+                    response.tvShowDetails.description,
+                    HtmlCompat.FROM_HTML_MODE_LEGACY
+                )
+
+                binding.tvReadMore.setOnClickListener {
+                    binding.tvTVShowDescription.maxLines = Int.MAX_VALUE
+                    binding.tvTVShowDescription.ellipsize = null
+                    binding.tvReadMore.visibility = View.GONE
+                }
+
+                binding.tvTVShowDescription.visibility = View.VISIBLE
+                binding.tvReadMore.visibility = View.VISIBLE
+
+                binding.tvRating.text = "%.2f".format(response.tvShowDetails.rating.toDouble())
+
+                if (response.tvShowDetails.genres.isNotEmpty()) {
+                    binding.tvGenres.text = response.tvShowDetails.genres.joinToString()
+                } else {
+                    binding.tvGenres.text = "N/A"
+                }
+
+                binding.tvRuntime.text = "${response.tvShowDetails.runtime} Min"
+
+                binding.llLayoutMisc.visibility = View.VISIBLE
+                binding.vDivider.visibility = View.VISIBLE
+                binding.vDivider2.visibility = View.VISIBLE
+
+                binding.btnWebsite.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse(response.tvShowDetails.url)
+                    startActivity(intent)
+                }
+
+                binding.btnEpisodes.visibility = View.VISIBLE
+                binding.btnWebsite.visibility = View.VISIBLE
+
+                loadBasicTVShowDetails(currentTVShow)
             }
         })
     }
@@ -69,7 +117,7 @@ class TVShowDetailsActivity : AppCompatActivity() {
         )
         layoutParams.setMargins(8, 0, 8, 0)
 
-        val indicators = Array(count) {
+        for (i in 0 until count) {
             val indicator = ImageView(applicationContext)
             indicator.setImageDrawable(ContextCompat.getDrawable(
                     applicationContext,
@@ -78,7 +126,6 @@ class TVShowDetailsActivity : AppCompatActivity() {
             )
             indicator.layoutParams = layoutParams
             binding.llSliderIndicators.addView(indicator)
-            indicator
         }
 
         binding.llSliderIndicators.visibility = View.VISIBLE
@@ -106,5 +153,17 @@ class TVShowDetailsActivity : AppCompatActivity() {
                 )
             }
         }
+    }
+
+    private fun loadBasicTVShowDetails(tvShow: TVShow) {
+        binding.tvTVShowTitle.text = tvShow.name
+        binding.tvTVShowNetwork.text = tvShow.network
+        binding.tvTVShowStatus.text = tvShow.status
+        binding.tvTVShowStarted.text = "Started on: ${tvShow.startDate}"
+
+        binding.tvTVShowTitle.visibility = View.VISIBLE
+        binding.tvTVShowNetwork.visibility = View.VISIBLE
+        binding.tvTVShowStarted.visibility = View.VISIBLE
+        binding.tvTVShowStatus.visibility = View.VISIBLE
     }
 }
